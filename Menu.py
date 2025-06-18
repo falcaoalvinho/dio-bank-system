93223b766dd69ac336c27f74d6909ab73707d2e7
Álvaro José <josealvaro3421@gmail.com>
2025-06-18 14:51:30 -0300

    Atualização do entregável, já funcional

---
+++ Menu.py
@@ -0,0 +1,9 @@
+# Função que exibe o menu de operações
+def printOpercoes() -> None:
+    print('MENU'.center(60, "="))
+    print(f'''
+        1) Depositar
+        2) Sacar
+        3) Sair
+    ''')
+    print('=' * 60)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Pacotes importados para o projeto
 import os
+import Menu
 
 # Constantes para Cores 
 # VERMELHO = '\033[31m'
@@ -7,23 +8,15 @@ import os
 # VERDE = '\033[32m'
 # FIM_COR = '\033m'
 
-# Função que exibe o menu de operações
-def printMenu() -> None:
-    print('MENU'.center(60, "="))
-    print(f'''
-        1) Depositar
-        2) Sacar
-        3) Sair
-    ''')
-    print('=' * 60)
-
 def contarCedulas(cedulas: dict) -> int:
     valor = 0
+
     for i in cedulas:
         valor += int(i) * cedulas[i]
+
     return valor
 
-def deposito(cedulas: dict, saldo: int) -> str:
+def depositar(cedulas: dict, saldo: int) -> {str, int}:
     cedulas_depositadas = {'200': 0, '100': 0, '50': 0, '20': 0, '10': 0, '5': 0, '2': 0}
 
     print('DEPOSITO'.center(60, '='))
@@ -36,35 +29,50 @@ def deposito(cedulas: dict, saldo: int) -> str:
                 break
             except ValueError:
                 print('\n   🚫[ERRO] Valor incorreto, por favor retorne um valor válido\n')
+                cedulas_depositadas[i] += int(input(f'Cédulas no valor de {i}: ')) 
+                cedulas[i] += cedulas_depositadas[i]
 
     valor_deposito = contarCedulas(cedulas_depositadas)
     saldo += valor_deposito
 
     print(f'\n    Saldo total: {saldo}')
     print(f'    Valor total do deposito: {valor_deposito}')
+    os.system('cls')
 
     return {'valor_deposito': f'\n    +{valor_deposito}', 'valor_saldo': saldo}
 
-def saque(cedulas: dict, saldo: int) -> str:
+def sacar(cedulas: dict, saldo: int, LIMITE_SAQUE: float) -> {str, int}:
+    cedulas_usadas = {'200': 0, '100': 0, '50': 0, '20': 0, '10': 0, '5': 0, '2': 0 }
+
     print('SAQUE'.center(60, '='))
+    print(f'   SAQUE MÁXIMO: {LIMITE_SAQUE}')
     print(f'   Saldo disponível: {saldo}')
 
-    while True:
-        try:
-            saque = int(input('\n   Digite o valor do saque: '))
-            valor_saque = saque
-        except ValueError:
-            print('\n🚫[ERRO] Valor incorreto, por favor retorne um valor válido\n')
-        else:
-            if saque <= saldo:
-                for i in cedulas:
-                    cedulas[i] -= saque // int(i)
-                    saque -= int(i) * (saque // int(i))
-                    saldo -= int(i) * (saque // int(i))
-                break
-            else:
-               print('\nValor indisponível para seu saldo') 
+    try:
+        saque = int(input('\n   Digite o valor do saque: '))
+        valor_saque = saque
 
+    except ValueError:
+        print('\n🚫[ERRO]Valor incorreto, por favor retorne um valor válido\n')
+        print(f'\n{'='*60}')
+        sacar(cedulas, saldo, LIMITE_SAQUE)
+
+    else:
+        if saque <= saldo and saque <= LIMITE_SAQUE:
+            for i in cedulas:
+                cedulas[i] -= saque // int(i)
+                cedulas_usadas[i] += saque // int(i)
+                saque -= int(i) * (saque // int(i))
+
+        else:
+            print('\n🚫[ERRO] Valor do saque excedeu o limite, por favor tente denovo')
+            print(f'\n{'='*60}')
+            sacar(cedulas, saldo, LIMITE_SAQUE)
+
+    print(cedulas_usadas)
+    saldo -= contarCedulas(cedulas_usadas)
+    
+    os.system('cls')
     return {'valor_saque': f'\n    -{valor_saque}', 'valor_saldo': saldo}
 
 # Função principal
@@ -79,30 +87,37 @@ def main() -> None:
     saldo = 1000
     saques_efetuados = 0
 
+    # Começo da construção do extrato
     extrato = 'EXTRATO'.center(60, '=')
     extrato += f'\nSALDO INICIAL: {saldo}'
-
-    print(f'BANCO DIO'.center(60, '='), '\n')
     
     while True:
-        printMenu()
+        os.system('cls')
+        Menu.printOpercoes()
 
         try:
             operacao_atual = int(input())
+
         except ValueError:
             print('🚫[ERRO] Valor incorreto, por favor retorne um valor válido')
+
         else:
             if operacao_atual == 1:
                 os.system('cls') # Limpa o terminal
-                resultado = deposito(cedulas, saldo)
+                resultado = depositar(cedulas, saldo)
                 extrato += resultado['valor_deposito']
-                saldo += resultado['valor_saldo']
-            elif operacao_atual == 2 and saques_efetuados <= LIMITE_SAQUE:
-                ++saques_efetuados
+                saldo = resultado['valor_saldo']
+
+            elif operacao_atual == 2 and saques_efetuados >= SAQUES_DIARIOS:
+                print('\n🚫[ERRO] Limite de saques diários atingido\n')
+
+            elif operacao_atual == 2:
+                saques_efetuados += 1
                 os.system('cls') # Limpa o terminal
-                resultado = saque(cedulas, saldo)
+                resultado = sacar(cedulas, saldo, LIMITE_SAQUE)
                 extrato += resultado['valor_saque']
-                saldo -= resultado['valor_saldo']
+                saldo = resultado['valor_saldo']
+                
             elif operacao_atual == 3:
                 os.system('cls') # Limpa o terminal
                 extrato += f'\nSALDO FINAL: {saldo}'
