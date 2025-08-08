af1f80083df2ed5842cb8a682eda50cf5b5f6c7a
Álvaro José <josealvaro3421@gmail.com>
2025-08-08 12:25:57 -0300

    build: criação do arquivo Cliente.py

--- Cliente.py
+++
@@ -1,8 +0,0 @@
-# Definição da classe cliente, contém apenas seu construtor e alguns atributos
-class cliente():
-    def __init__(self, nome: str, data_nascimento: str, cpf: str, endereco: str):
-        self.nome: str = nome;
-        self.data_nascimento: str = data_nascimento
-        self.cpf: str = cpf
-        self.endereco: str = endereco
-        self.contas: list = []
