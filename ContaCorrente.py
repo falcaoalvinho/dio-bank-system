4304fbfee4298138a2ad1fb5f41a13ce9b3d56cc
Álvaro José <josealvaro3421@gmail.com>
2025-08-08 15:30:02 -0300

    style: adicionado retorno das funções para classe contaCorrente

--- ContaCorrente.py
+++ ContaCorrente.py
@@ -1,24 +1,21 @@
-from Conta import Conta
-from Cliente import Cliente
-from Historico import Historico
+from Conta import conta
+from Cliente import cliente
+from Historico import historico
 
-class ContaCorrente(Conta):
-    def __init__(
-            self,
-            saldo: float,
-            numero: int,
-            agencia: str,
-            cliente: Cliente,
-            historico: Historico,
-            limite: float,
-            limite_saques: int
-            ):
-        super().__init__(saldo, numero, agencia, cliente, historico)
-        self.__limite = limite
-        self.__limite_saques = limite_saques
+class contaCorrente(conta):
+    def __init__(self,):
+        super().__init__()
+        self.__limite        = None
+        self.__limite_saques = None
 
-    def set_limite(self, limite: float):
+    def get_limite(self) -> float:
+        return self.__limite
+    
+    def get_limite_saques(self) -> float:
+        return self.__limite_saques
+
+    def set_limite(self, limite: float) -> None:
         self.__limite = limite
 
-    def set_limite_saques(self, limite_saques: int):
+    def set_limite_saques(self, limite_saques: int) -> None:
         self.__limite_saques = limite_saques
