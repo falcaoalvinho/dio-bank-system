d7c6e9d94dc90484595624d2a69d2ffeea16cd50
Álvaro José <josealvaro3421@gmail.com>
2025-08-08 12:47:50 -0300

    feat: métodos setters para dados para a classe

--- PessoaFisica.py
+++ PessoaFisica.py
@@ -2,6 +2,18 @@ from datetime import date
 
 class PessoaFisica:
     def __init__(self, cpf: str, nome: str, data_nascimento: date):
-        self.cpf             = cpf
-        self.nome            = nome
-        self.data_nascimento = data_nascimento
+        self.__cpf             = cpf
+        self.__nome            = nome
+        self.__data_nascimento = data_nascimento
+
+    def set_cpf(self, cpf: str) -> None:
+        self.__cpf = cpf
+
+    def set_nome(self, nome: str) -> None:
+        self.__nome = nome
+
+    def set_data_nascimento(self, data_nascimento: date) -> None:
+        self.__data_nascimento = data_nascimento
+
+
+    
